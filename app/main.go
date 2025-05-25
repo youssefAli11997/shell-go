e172934b070d1f4409034736ab252c979c8934d2
Youssef <youssef@Youssefs-MacBook-Air.local>
2025-05-25 22:11:47 +0200

    echo command

--- app/main.go
+++ app/main.go
@@ -31,11 +31,17 @@ func evaluateCommandLine(commandLine string) {
 
 	switch command {
 	case "exit":
+		if len(commandLineParts) == 1 {
+			// If no argument is provided, just exit
+			os.Exit(0)
+		}
 		errorCode, parsingError := strconv.ParseInt(commandLineParts[1], 10, 32)
 		if parsingError != nil {
 			os.Exit(0)
 		}
 		os.Exit(int(errorCode))
+	case "echo":
+		fmt.Println(strings.Join(commandLineParts[1:], " "))
 	default:
 		fmt.Println(command + ": command not found")
 	}
