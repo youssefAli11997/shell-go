2d53387755d37cc804bf1eb23cc100bf716cf6c7
Youssef <youssef@Youssefs-MacBook-Air.local>
2025-05-25 18:49:26 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,9 +7,15 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	// Remove trailing newline character
+	command = command[:len(command)-1]
+	println(command + ": command not found")
 }
