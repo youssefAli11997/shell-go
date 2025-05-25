99a52fabcb51eb0583bfc891c048265a70f6a5dd
Youssef <youssef@Youssefs-MacBook-Air.local>
2025-05-25 18:56:30 +0200

    REPL

--- app/main.go
+++ app/main.go
@@ -7,15 +7,17 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		// Remove trailing newline character
+		command = command[:len(command)-1]
+		println(command + ": command not found")
 	}
-	// Remove trailing newline character
-	command = command[:len(command)-1]
-	println(command + ": command not found")
 }
